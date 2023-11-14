6bfb469c55f55b8509f4d478f45167232a52ab43
Wmatistic <Johnmatistic@gmail.com>
2023-11-14 02:00:15 -0600

    ALL LEGS MOVING YIPPIE

--- software/OrrinMove/OrrinMove.ino
+++ software/OrrinMove/OrrinMove.ino
@@ -15,33 +15,33 @@ Copy Paste Movement Input
 //      LEGS
 const double thighLength = 6.5;
 const double calfLength = 6.5;
-const double maxY = calfLength + thighLength;
+const double maxY = 12.8;
 double angles[4][3];
 
 //  Front Left LEG
 const int FLThigh = 0;
-const double FLThighOffset = 0.9;
+const double FLThighOffset = 1.0;
 const int FLCalf = 1;
 const double FLCalfOffset = 1.0;
 const int FLHip = 2;
 
 //  Front Right LEG
 const int FRThigh = 3;
-const double FRThighOffset = 1.0;
+const double FRThighOffset = 1.2;
 const int FRCalf = 4;
-const double FRCalfOffset = 1.2;
+const double FRCalfOffset = 1.0;
 const int FRHip = 5;
 
 //  Back Left LEG
 const int BLThigh = 6;
-const double BLThighOffset = 1.2;
+const double BLThighOffset = 1.0;
 const int BLCalf = 7;
-const double BLCalfOffset = 1.;
+const double BLCalfOffset = 1.0;
 const int BLHip = 8;
 
 // Back Right LEG
 const int BRThigh = 9;
-const double BRThighOffset = 1.0;
+const double BRThighOffset = 1.2;
 const int BRCalf = 10;
 const double BRCalfOffset = 1.0;
 const int BRHip = 11;
@@ -73,15 +73,16 @@ void loop() {
     tempX += 0.3;
   }
 
-  moveOrrin(tempX, tempY, tempR,
+  moveOrrin(0, tempY, tempR,
             0, tempY, 150+tempR,
             0, tempY, tempR,
-            0, 0, 0);
+            0, tempY, tempR);
 
+  Serial.print(tempY);
   Serial.print("thigh: ");
-  Serial.println(convertThighAngleToServo(angles[1][0]));
+  Serial.println(convertThighAngleToServo(angles[3][0]));
   Serial.print("calf: ");
-  Serial.println(convertCalfAngleToServo(angles[1][1]));
+  Serial.println(convertCalfAngleToServo(angles[3][1]));
   Serial.println(tempR);
   //Serial.println(angles[1][2]);
 }
@@ -103,40 +104,40 @@ void moveOrrin(double x0, double y0, double r0,
   // THIGH
   HCPCA9685.Servo(FLThigh, altConvertThighAngleToServo(angles[0][0]) * FLThighOffset);
   // CALF
-  HCPCA9685.Servo(FLCalf, 90);
+  HCPCA9685.Servo(FLCalf, (convertCalfAngleToServo(angles[0][1]) + 70) * FRCalfOffset);
   // HIP
   HCPCA9685.Servo(FLHip, angles[0][2]);
 
   //  Front Right LEG
   // THIGH
-  HCPCA9685.Servo(FRThigh, convertThighAngleToServo(angles[1][0]));
+  HCPCA9685.Servo(FRThigh, convertThighAngleToServo(angles[1][0]) * FRThighOffset);
   // CALF
-  HCPCA9685.Servo(FRCalf, altConvertCalfAngleToServo(angles[1][1]));
+  HCPCA9685.Servo(FRCalf, altConvertCalfAngleToServo(angles[1][1]) * FRCalfOffset);
   // HIP
   HCPCA9685.Servo(FRHip, angles[1][2]);
 
   //  Back Left LEG
   // THIGH
-  HCPCA9685.Servo(BLThigh, convertThighAngleToServo(angles[2][0]) * BLThighOffset);
+  HCPCA9685.Servo(BLThigh, altConvertThighAngleToServo(angles[2][0]) * BLThighOffset);
   // CALF
-  HCPCA9685.Servo(BLCalf, altConvertCalfAngleToServo(angles[2][1]) * BLCalfOffset);
+  HCPCA9685.Servo(BLCalf, (convertCalfAngleToServo(angles[2][1]) + 70) * BLCalfOffset);
   // HIP
   HCPCA9685.Servo(BLHip, angles[2][2]);
 
-//  //  Back Right LEG
-//  // THIGH
-//  HCPCA9685.Servo(BRThigh, convertThighAngleToServo(angles[3][0]));
-//  // CALF
-//  HCPCA9685.Servo(BRCalf, convertCalfAngleToServo(angles[3][1]));
-//  // HIP
-//  HCPCA9685.Servo(BRHip, angles[3][2]);
+  //  Back Right LEG
+  // THIGH
+  HCPCA9685.Servo(BRThigh, (convertThighAngleToServo(angles[3][0]) + 60) * BRThighOffset);
+  // CALF
+  HCPCA9685.Servo(BRCalf, altConvertCalfAngleToServo(angles[3][1]) * BRCalfOffset);
+  // HIP
+  HCPCA9685.Servo(BRHip, angles[3][2]);
 
   
 }
 
 void calculateLegAngles(double pos[4][3]) {
 
-  for (int i = 0; i <= sizeof(pos); i++){
+  for (int i = 0; i <= sizeof(pos)+1; i++){
     double x = pos[i][0];
     double y = pos[i][1];
     double r = pos[i][2];
